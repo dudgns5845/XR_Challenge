a2edd8949c4ae86d43e8ac4d0e0ebe11b2740bea
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-10-12 11:16:31 +0900

    Update Rio

--- GLARE/Assets/Rio/Scripts/PrintWorkOutInfo_Rio.cs
+++ GLARE/Assets/Rio/Scripts/PrintWorkOutInfo_Rio.cs
@@ -2,63 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 class WorkOutInfo
 {
-    
     public int idx;
     public string name;
     public string explain;
     public string tip;
 
-    public WorkOutInfo(int idx , string name , string explain, string tip)
+    public WorkOutInfo(int idx, string name, string explain, string tip)
     {
         this.idx = idx;
         this.name = name;
         this.explain = explain;
         this.tip = tip;
     }
+}
 
+[System.Serializable]
+class JsonArrayData
+{
+    public List<WorkOutInfo> d;
 }
 
 
 public class PrintWorkOutInfo_Rio : MonoBehaviour
 {
-    //void Start()
-    //{
-    //    List<Dictionary<string, object>> data = CSVReader.Read("WorkOutInfo");
-    //    for(int i = 0; i < data.Count; i++)
-    //    {
-    //        print(data[i]["Name"].ToString() + " : " + data[i]["Explain"].ToString());
-    //    }
-
-    //}
-
+    WorkOutInfo aa;
     List<WorkOutInfo> info;
 
     //운동정보를 csv에서 읽어와 객체에 저장하고 리스트를 만드는 작업
     private void Start()
     {
+        info = new List<WorkOutInfo>();
         //csv에서 읽어와서
         List<Dictionary<string, object>> data = CSVReader.Read("WorkOutInfo");
         //객체 생성하기
         for (int i = 0; i < data.Count; i++)
         {
-            int idx = int.Parse(data[i]["Number"].ToString());
+            int idx = 0;//int.Parse(data[i]["Number"].ToString());
             string name = data[i]["Name"].ToString();
             string explain = data[i]["Explain"].ToString();
             string tip = data[i]["Tip"].ToString();
 
-            //print(name);
-            info.Add( new WorkOutInfo(idx, name, explain, tip));
-            //print(data[i]["Number"].ToString()+" : " + data[i]["Name"].ToString() + " : " + data[i]["Explain"].ToString() + " : "+ data[i]["Tip"].ToString());
-            //info.Add(new WorkOutInfo(int.Parse(data[i]["Number"].ToString()), data[i]["Name"].ToString(), data[i]["Explain"].ToString(), data[i]["Tip"].ToString()));
+            info.Add(new WorkOutInfo(idx, name, explain, tip));
         }
-
-        //for(int i = 0; i < info.Count; i++)
-        //{
-        //    print(info[i].name);
-        //}
+        JsonArrayData jsonData = new JsonArrayData();
+        jsonData.d = info;
+        string jsonString = JsonUtility.ToJson(jsonData, true);
+        print(jsonString);
     }
-
-
 }
